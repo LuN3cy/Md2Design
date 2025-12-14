1dae8c3a9218faaa87b6b1e6fd06ce5b0540aef8
LuN3cy <1536968883@qq.com>
2025-12-14 17:17:29 +0800

    New version

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Md2Design/',
+  // If TAURI_PLATFORM env is defined, use base './', otherwise use '/Md2Design/' for GH Pages
+  base: process.env.TAURI_PLATFORM ? './' : '/Md2Design/',
   plugins: [
     react(),
     tailwindcss(),
